80ed3f2a452c79e9baa8d75da51abf467f836481
forestnoobie <nakyilkim@naver.com>
2020-05-20 10:29:52 +0900

    add notebooks, mod gitignore

---
+++ setup.sh
@@ -0,0 +1,21 @@
+conda install -c anaconda pandas
+y
+conda install -c conda-forge matplotlib
+y
+conda install -c anaconda numpy
+y
+conda install -c plotly plotly
+y
+conda install -c conda-forge tqdm
+y
+
+conda install -c anaconda ipykernel
+y
+
+conda install -c anaconda networkx
+y
+
+conda install pytorch==1.2.0 torchvision==0.4.0 -c pytorch
+y
+
+python -m ipykernel install --user --name=auto_ml
